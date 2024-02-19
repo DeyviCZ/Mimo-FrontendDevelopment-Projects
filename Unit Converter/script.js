19ebf1ce1097ffba9ac9a21482088575bf14bb7a
Deyvi Correa Zamora <88319480+Daviz-357@users.noreply.github.com>
2024-02-19 18:27:01 -0300

    Update script.js

--- Unit Converter/script.js
+++ Unit Converter/script.js
@@ -1,17 +1,21 @@
 
 function convert(){
-   let valorInput = document.getElementById("valorInput");
-   let unidadSelect = document.getElementById("unidadSelect");
-   let result = document.getElementById("result");
+    let valorInput = document.getElementById("valorInput");
+    let unidadSelect = document.getElementById("unidadSelect");
+    let result = document.getElementById("result");
 
-   let valor = valorInput.value;
-   let unidad = unidadSelect.value;
+    let valor = valorInput.value;
+    let unidad = unidadSelect.value;
 
-    if(unidad === "miles"){
-        let kilometers = valor / 0.621371;
-        result.innerText = `Result: ${kilometers.toFixed(3)}`;
-    } else if(unidad === "kilometers"){
-        let miles = valor * 0.621371;
-        result.innerHTML = `Result: ${miles.toFixed(3)}`;
+    if(valor === "" || valor.trim() === ""){
+        return alert('Insert Number to Convert')
+    } else{
+        if(unidad === "miles"){
+            let kilometers = valor / 0.621371;
+            result.innerText = `Result: ${kilometers.toFixed(3)}`;
+        } else if(unidad === "kilometers"){
+            let miles = valor * 0.621371;
+            result.innerHTML = `Result: ${miles.toFixed(3)}`;
+        }
     }
 }
